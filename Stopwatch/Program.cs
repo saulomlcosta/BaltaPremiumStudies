abe2a489c748b92bcc5b69b2c4659e4dbdd5e53a
Saulo Costa <79532045+saulomlcosta@users.noreply.github.com>
2023-01-18 23:45:51 -0300

    Update Repository

---
+++ Stopwatch/Program.cs
@@ -0,0 +1,35 @@
+﻿using static System.Console;
+
+Menu();
+
+static void Menu()
+{
+    WriteLine("-----------StopWatch------------");
+    WriteLine("S => Second => 10s = 10 seconds");
+    WriteLine("M => Minute => 3m = 3 minutes");
+    WriteLine("H => Hour => 7h = 7 hours");
+    WriteLine("0 => Exit");
+    WriteLine("How long do you wanna count?");
+
+    string data = ReadLine().ToLower();
+    char type = char.Parse(data.Substring(data.Length - 1, 1));
+    int time = int.Parse(data.Substring(0, data.Length - 1));
+
+    WriteLine(type);
+    WriteLine(time);
+    ReadLine();
+}
+
+static void Start()
+{
+    int time = 10;
+    int currentTime = 0;
+
+    while (currentTime != time)
+    {
+        Clear();
+        currentTime++;
+        WriteLine(currentTime);
+        Thread.Sleep(1000);
+    }
+}
---
+++ calculator/Program.cs
@@ -0,0 +1,119 @@
+﻿using static System.Console;
+
+Menu();
+
+static void Menu()
+{
+    Clear();
+
+    Console.ForegroundColor
+    = ConsoleColor.Blue;
+    WriteLine("-----------------------Calculator-------------------------------");
+    WriteLine("1 - Calculate");
+    WriteLine("2 - Subtract");
+    WriteLine("3 - Divide");
+    WriteLine("4 - Multiply");
+    WriteLine("5 - Exit");
+    WriteLine("----------------------------------------------------------------");
+    WriteLine("");
+
+    bool optionValid = short.TryParse(ReadLine(), out short option);
+
+    if (optionValid)
+    {
+
+        Console.ForegroundColor
+              = ConsoleColor.Gray;
+
+        switch (option)
+        {
+            case 1: Sum(); break;
+            case 2: Sub(); break;
+            case 3: Div(); break;
+            case 4: Mult(); break;
+            case 5: System.Environment.Exit(0); break;
+            default: Menu(); break;
+        }
+    }
+    else
+    {
+        Console.ForegroundColor
+       = ConsoleColor.Red;
+
+        WriteLine("Please enter a valid option");
+
+        WriteLine("");
+
+        Console.ForegroundColor
+       = ConsoleColor.Yellow;
+        WriteLine("Enter to return to the Menu");
+        ReadKey();
+        Menu();
+    }
+}
+
+static void Sum()
+{
+    WriteLine("What your first number?");
+    float n1 = float.Parse(ReadLine());
+    WriteLine("");
+    WriteLine("And your second number?");
+    float n2 = float.Parse(ReadLine());
+
+    Console.ForegroundColor
+       = ConsoleColor.Green;
+
+    WriteLine($"Your result is {n1 + n2}");
+    ReadKey();
+    Menu();
+}
+
+static void Sub()
+{
+    WriteLine("What your first number?");
+    float n1 = float.Parse(ReadLine());
+    WriteLine("");
+    WriteLine("And your second number?");
+    float n2 = float.Parse(ReadLine());
+
+    Console.ForegroundColor
+          = ConsoleColor.Green;
+    WriteLine($"Your result is {n1 - n2}");
+    ReadKey();
+    Menu();
+}
+
+static void Div()
+{
+    WriteLine("What your first number?");
+    float n1 = float.Parse(ReadLine());
+    WriteLine("");
+    WriteLine("And your second number?");
+    float n2 = float.Parse(ReadLine());
+
+    Console.ForegroundColor
+          = ConsoleColor.Green;
+
+    WriteLine($"Your result is {n1 / n2}");
+    ReadKey();
+    Menu();
+}
+
+static void Mult()
+{
+    WriteLine("What your first number?");
+    float n1 = float.Parse(ReadLine());
+    WriteLine("");
+    WriteLine("And your second number?");
+    float n2 = float.Parse(ReadLine());
+
+    Console.ForegroundColor
+          = ConsoleColor.Green;
+
+    WriteLine($"Your result is {n1 * n2}");
+    ReadKey();
+    Menu();
+}
+
+
+
